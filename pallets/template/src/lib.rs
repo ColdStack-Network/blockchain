8080649b3948c8b6e59e3454c4d76284d4694aa6
Ubuntu <ubuntu@ip-172-31-20-167.ap-southeast-1.compute.internal>
2021-02-12 14:19:18 +0000

    remove dead code

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -5,9 +5,6 @@ use frame_support::{
 };
 use frame_system::ensure_signed;
 use sp_std::vec::Vec;
-//TODO remove
-use frame_support::sp_runtime::print;
-use frame_support::debug;
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
@@ -15,68 +12,13 @@ pub trait Trait: frame_system::Trait {
     type Event: From<Event/*<Self>*/> + Into<<Self as frame_system::Trait>::Event>;
 }
 
-//type Balance = u128;
-
-
-// Pallets use events to inform users when important changes are made.
-// Event documentation should end with an array that provides descriptive names for parameters.
-// https://substrate.dev/docs/en/knowledgebase/runtime/events
-decl_event! {
-    pub enum Event/*<T> where AccountId = <T as frame_system::Trait>::AccountId*/ {
-        /*
-        /// Event emitted when a proof has been claimed. [who, claim]
-        ClaimCreated(AccountId, Vec<u8>),
-        /// Event emitted when a claim is revoked by the owner. [who, claim]
-        ClaimRevoked(AccountId, Vec<u8>),
-        Deposit(Vec<u8>, u128),
-        */
-        PayForUpload(
-          Vec<u8>, /* file id */
-          Vec<u8>, /* owner */
-          Vec<u8>, /* filename */
-          u128,    /* size bytes */
-        ),
-    }
-}
-
-
-// Errors inform users that something went wrong.
-decl_error! {
-    pub enum Error for Module<T: Trait> {
-        /*
-        /// The proof has already been claimed.
-        ProofAlreadyClaimed,
-        /// The proof does not exist, so it cannot be revoked.
-        NoSuchProof,
-        /// The proof is claimed by another account, so caller can't revoke it.
-        NotProofOwner,
-        InsufficientBalance,
-        */
-        Unauthorized
-    }
-}
 
 // The pallet's runtime storage items.
 // https://substrate.dev/docs/en/knowledgebase/runtime/storage
 decl_storage! {
     trait Store for Module<T: Trait> as TemplateModule {
         Key get(fn key) config(): T::AccountId;
-        /*
-        /// The storage item for our proofs.
-        /// It maps a proof to the user who made the claim and when they made it.
-        Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
-        Balances: map hasher(blake2_128_concat) Vec<u8> => Balance;
-        */
     }
-    /*
-    add_extra_genesis {
-      config(genesis_field): GenesisFieldType;
-      config(genesis_field2): GenesisFieldType;
-      build(|_: &Self| {
-      // Modification of storage
-      })
-    }
-    */
 }
 
 // Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -90,87 +32,36 @@ decl_module! {
         // Events must be initialized if they are used by the pallet.
         fn deposit_event() = default;
 
-        /*
-        /// Allow a user to claim ownership of an unclaimed proof.
-        #[weight = 10_000]
-        fn create_claim(origin, proof: Vec<u8>) {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-            let sender = ensure_signed(origin)?;
-
-            // Verify that the specified proof has not already been claimed.
-            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
-
-            // Get the block number from the FRAME System module.
-            let current_block = <frame_system::Module<T>>::block_number();
-
-            // Store the proof with the sender and block number.
-            Proofs::<T>::insert(&proof, (&sender, current_block));
-
-            // Emit an event that the claim was created.
-            Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
-        }
-
-        /// Allow the owner to revoke their claim.
-        #[weight = 10_000]
-        fn revoke_claim(origin, proof: Vec<u8>) {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-            let sender = ensure_signed(origin)?;
-
-            // Verify that the specified proof has been claimed.
-            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
-
-            // Get owner of the claim.
-            let (owner, _) = Proofs::<T>::get(&proof);
-
-            // Verify that sender of the current call is the claim owner.
-            ensure!(sender == owner, Error::<T>::NotProofOwner);
-
-            // Remove claim from storage.
-            Proofs::<T>::remove(&proof);
-
-            // Emit an event that the claim was erased.
-            Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
-        }
-
-        #[weight = 10_000]
-        fn deposit(origin, acc: Vec<u8>, balance: Balance) {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-            let sender = ensure_signed(origin)?;
-
-            // TODO verify acc length
-
-            // TODO: verify sender
-
-            // Get owner of the claim.
-            //let b = Balances::get(acc);
-
-            Balances::insert(&acc, balance);
-
-            Self::deposit_event(RawEvent::Deposit(acc, balance));
-        }
-        */
-
         #[weight = 10_000]
         // TODO u128 for size?
         fn pay_for_upload(origin, id: Vec<u8>, owner: Vec<u8>, name: Vec<u8>, size_bytes: u128){
           let sender = ensure_signed(origin)?;
-          //ensure!(1 == 2, Error::<T>::Unauthorized);
           ensure!(sender == Self::key(), Error::<T>::Unauthorized);
           // TODO verify acc length
-          // TODO: verify sender
-          // print("Key {}");
-          // print(Self::key());
-          //debug::info!("Key {}", Self::key());
           // TODO: check args
           Self::deposit_event(/*Raw*/Event::PayForUpload(id, owner, name, size_bytes));
         }
     }
 }
 
+// Pallets use events to inform users when important changes are made.
+// Event documentation should end with an array that provides descriptive names for parameters.
+// https://substrate.dev/docs/en/knowledgebase/runtime/events
+decl_event! {
+    pub enum Event/*<T> where AccountId = <T as frame_system::Trait>::AccountId*/ {
+        PayForUpload(
+          Vec<u8>, /* file id */
+          Vec<u8>, /* owner */
+          Vec<u8>, /* filename */
+          u128,    /* size bytes */
+        ),
+    }
+}
 
+
+// Errors inform users that something went wrong.
+decl_error! {
+    pub enum Error for Module<T: Trait> {
+        Unauthorized
+    }
+}
