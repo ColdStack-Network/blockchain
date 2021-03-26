2bb355199813b2342b8e6def8ce97493a2c34691
Ubuntu <ubuntu@ip-172-31-20-167.ap-southeast-1.compute.internal>
2021-03-26 08:12:41 +0000

    WIP

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -280,7 +280,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment::{Module, Storage},
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
-		ColdStack: pallet_coldstack::{Module, Call, Storage, Event/*<T>*/, Config<T>},
+		ColdStack: pallet_coldstack::{Module, Call, Storage, Event<T>, Config<T>},
 	}
 );
 
