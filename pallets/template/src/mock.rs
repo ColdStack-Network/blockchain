b848a4be711e4cdd36e0f62dda4887ade482cc69
Dmitry Vasil <dmitry.vasil@gmail.com>
2021-04-06 16:33:51 +0000

    fix

--- pallets/template/src/mock.rs
+++ pallets/template/src/mock.rs
@@ -7,7 +7,7 @@ use sp_runtime::{
 use frame_system as system;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>; 
+type Block = frame_system::mocking::MockBlock<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
@@ -17,7 +17,7 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system::{Module, Call, Config, Storage, Event<T>},
-		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		ColdStack: pallet_template::{Module, Call, Storage, Event<T>},
 	}
 );
 
