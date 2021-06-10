1eda70707b409ced724f5c740a071a8000473398
Dmitry Vasilev <dmitry.vasil@gmail.com>
2021-06-11 06:45:56 +0800

    fix unused warning

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -130,7 +130,7 @@ fn testnet_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
+	_endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
 ) -> GenesisConfig {
   let root_key_clone = root_key.clone();
