2f39ac7d24bfdf606763de27f03ff2c29c5d1292
Dmitry Vasilev <dmitry.vasil@gmail.com>
2021-06-23 11:07:52 +0000

    remove comments from substrate-node-template

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,8 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 pub use pallet::*;
 
 #[cfg(test)]
@@ -21,10 +18,8 @@ pub mod pallet {
   use frame_system::pallet_prelude::*;
   use sp_std::vec::Vec;
 
-  /// Configure the pallet by specifying the parameters and types on which it depends.
   #[pallet::config]
   pub trait Config: frame_system::Config {
-    /// Because this pallet emits events, it depends on the runtime's definition of an event.
     type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
   }
 
@@ -32,12 +27,10 @@ pub mod pallet {
   #[pallet::generate_store(pub(super) trait Store)]
   pub struct Pallet<T>(_);
 
-  // The pallet's runtime storage items.
-  // https://substrate.dev/docs/en/knowledgebase/runtime/storage
   #[pallet::storage]
   #[pallet::getter(fn key)]
-  // Learn more about declaring storage items:
-  // https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
+
+  // Admin account key
   pub type Key<T: Config> = StorageValue<_, T::AccountId, ValueQuery>;
 
   #[pallet::storage]
@@ -152,15 +145,10 @@ pub mod pallet {
     }
   }
 
-  // Pallets use events to inform users when important changes are made.
-  // https://substrate.dev/docs/en/knowledgebase/runtime/events
   #[pallet::event]
   #[pallet::metadata(T::AccountId = "AccountId")]
   #[pallet::generate_deposit(pub(super) fn deposit_event)]
   pub enum Event<T: Config> {
-    /// Event documentation should end with an array that provides descriptive names for event
-    /// parameters. [something, who]
-    // TODO pass custom struct to event?
     Upload(
       /*user_eth_address*/     Vec<u8>,
       /*file_name_hash:*/      Vec<u8>,
@@ -192,7 +180,6 @@ pub mod pallet {
     GatewayNodeRegistered(Vec<u8>, Option<Vec<u8>>, Vec<u8>),
   }
 
-  // Errors inform users that something went wrong.
   #[pallet::error]
   pub enum Error<T> {
     InvalidArguments,
@@ -204,12 +191,6 @@ pub mod pallet {
   #[pallet::hooks]
   impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-
-
-
-  // Dispatchable functions allows users to interact with the pallet and invoke state changes.
-  // These functions materialize as "extrinsics", which are often compared to transactions.
-  // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
   #[pallet::call]
   impl<T: Config> Pallet<T> {
 
