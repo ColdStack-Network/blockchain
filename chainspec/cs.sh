ed047b9be03316298432f3ee81d60256e486036b
Dmitry Vasilev <dmitry.vasil@gmail.com>
2021-07-22 20:34:18 +0800

    remove file added by accident

--- chainspec/cs.sh
+++
@@ -1,1 +0,0 @@
-./create_chainspec.py --chainspec staging/chainspec.json --rawchainspec staging/chainspecRaw.json --secrets ../deploy/secrets/staging.json --name "ColdStack staging" --id coldstack-staging
